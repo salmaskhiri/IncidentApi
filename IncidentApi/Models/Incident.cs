8b04983c8b0414c5ee20e25ff5fe293357ea631e
salma <salmaskhiri7@gmail.com>
2026-02-15 15:48:39 +0100

    Ajoutez des fichiers projet.

---
+++ IncidentApi/Models/Incident.cs
@@ -0,0 +1,21 @@
+﻿using System.Data;
+using System.ComponentModel.DataAnnotations;
+namespace IncidentApi.Models
+
+{
+    public class Incident
+    {
+        public int Id { get; set; } 
+        [Required]
+        [StringLength(30, MinimumLength = 3,ErrorMessage="erreur")]
+        public string Title { get; set; } = string.Empty;
+        [Required]
+        [StringLength(200)]
+        public string Description { get; set; } = string.Empty;
+        [Required]
+        public string Severity { get; set; } = string.Empty;
+       
+        public string Status { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+    }
+}
