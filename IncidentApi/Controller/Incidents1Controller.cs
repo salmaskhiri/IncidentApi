e064d14ce347b6d44dc3d8183f26cea6ae987f62
salma <salmaskhiri7@gmail.com>
2026-03-02 10:48:33 +0100

    integration de la base de donnees et ajout du dbcontext et migrations

---
+++ IncidentApi/Controller/Incidents1Controller.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using IncidentApi.Models;
+
+namespace IncidentApi.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Incidents1Controller : ControllerBase
+    {
+        private readonly IncidentsDbContext _context;
+
+        public Incidents1Controller(IncidentsDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Incidents1
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Incident>>> GetIncident()
+        {
+            return await _context.Incident.ToListAsync();
+        }
+
+        // GET: api/Incidents1/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Incident>> GetIncident(int id)
+        {
+            var incident = await _context.Incident.FindAsync(id);
+
+            if (incident == null)
+            {
+                return NotFound();
+            }
+
+            return incident;
+        }
+
+        // PUT: api/Incidents1/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutIncident(int id, Incident incident)
+        {
+            if (id != incident.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(incident).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!IncidentExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Incidents1
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Incident>> PostIncident(Incident incident)
+        {
+            _context.Incident.Add(incident);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetIncident", new { id = incident.Id }, incident);
+        }
+
+        // DELETE: api/Incidents1/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteIncident(int id)
+        {
+            var incident = await _context.Incident.FindAsync(id);
+            if (incident == null)
+            {
+                return NotFound();
+            }
+
+            _context.Incident.Remove(incident);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool IncidentExists(int id)
+        {
+            return _context.Incident.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ IncidentApi/Models/IncidentsDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using IncidentApi.Models;
+
+namespace IncidentApi.Modelsss
+{
+    public class IncidentsDbContext : DbContext
+    {
+        public IncidentsDbContext(DbContextOptions<IncidentsDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<IncidentApi.Models.Incident> Incident { get; set; } = default!;
+
+        // Exemple :
+        // public DbSet<Incident> Incidents { get; set; }
+    }
+}
